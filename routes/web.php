e9d67fdd65be3760bf2b261c3014895aa1d819b8
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-10-05 08:45:55 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::resource('transactions', TransactionController::class);
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
